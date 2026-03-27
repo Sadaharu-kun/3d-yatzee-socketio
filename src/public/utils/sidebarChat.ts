f9624c08feaffe2e149c8d8a8583ed25282b63fd
Melvin <melleos@icloud.com>
2026-03-27 22:03:45 +0100

    Made sidebar utils. Gjorde en import för att inte få problem med olika inladdade skript

---
+++ src/public/utils/sidebarChat.ts
@@ -0,0 +1,53 @@
+export function initSidebarChat() {
+    console.log('INIT Sidebar Chat');
+
+    const sidebarChatContainer = document.querySelector('#sidebar-chat-container') as HTMLElement;
+
+    if (!sidebarChatContainer) console.error('sidebarCharContainer finns inte');
+
+    const handleMouseEnter = () => expandSidebar(sidebarChatContainer);
+    const handleMouseLeave = () => collapseSidebar(sidebarChatContainer);
+
+    sidebarChatContainer.addEventListener('mouseenter', handleMouseEnter);
+    sidebarChatContainer.addEventListener('mouseleave', handleMouseLeave);
+}
+
+export function expandSidebar(sidebarChatContainer: HTMLElement): void {
+    // console.debug('🪳 expandSidebar()');
+
+    sidebarChatContainer.classList.remove('w-24');
+    sidebarChatContainer.classList.add('w-1/3');
+    sidebarChatContainer.classList.add('h-screen');
+}
+export function collapseSidebar(sidebarChatContainer: HTMLElement): void {
+    // console.debug('🪳 collapseSidebar()');
+
+    sidebarChatContainer.classList.add('w-24');
+    sidebarChatContainer.classList.remove('w-1/3');
+    sidebarChatContainer.classList.remove('h-screen');
+}
+
+export function toggleSidebar(sidebarChatContainer: HTMLElement): void {
+    // console.debug('🪳 toggleSidebar()');
+
+    /*
+    1. Small chat (w-24)
+    2. HOVER --> w-24 h-screen
+*/
+
+    // w-14 is start-size
+    if (sidebarChatContainer.classList.contains('w-24')) {
+        // console.debug('🪳 IS w-24');
+        // Expand
+        sidebarChatContainer.classList.add('w-1/2');
+
+        sidebarChatContainer.classList.remove('w-24');
+        sidebarChatContainer.classList.remove('h-screen');
+    } else {
+        // Collapse
+        sidebarChatContainer.classList.remove('w-1/2');
+
+        sidebarChatContainer.classList.add('w-24');
+        sidebarChatContainer.classList.add('h-screen');
+    }
+}
